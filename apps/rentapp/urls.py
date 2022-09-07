d7b74f44f35e230b8a3adecf7679489841194f95
Sunil Thapa <tsunil359@gmail.com>
2022-09-07 11:38:41 +0545

    use defaultrouter instead

--- apps/rentapp/urls.py
+++ apps/rentapp/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from .views import  TenantViewSet, NotificationViewSet, RentViewSet
 
 
 
-router = SimpleRouter()
-router.register('tenants', TenantViewSet, basename= 'tenants')
-
-
-
+router = DefaultRouter()
+router.register('my-tenants/', TenantViewSet, basename= 'tenants')
 router.register('rent', RentViewSet, basename= 'rent')
-
 
 # router.register('complaints', ComplaintViewSet, basename= 'complaint')
 router.register('notifications', NotificationViewSet, basename= 'notification')

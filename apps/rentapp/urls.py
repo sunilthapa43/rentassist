e993ac35b8d7aef068126d0c8eb2827c7a0ae849
Sunil Thapa <tsunil359@gmail.com>
2022-09-10 15:04:39 +0545

    add rent viewsets

--- apps/rentapp/urls.py
+++ apps/rentapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CompalaintViewSet
+from .views import CompalaintViewSet, RentViewSet
 
 
 
 router = DefaultRouter()
-# router.register('rent', RentViewSet, basename= 'rent')
+router.register('rent', RentViewSet, basename= 'rent')
 
 router.register('complaints',CompalaintViewSet, basename= 'complaint')
 # router.register('notifications', NotificationViewSet, basename= 'notification')
--- apps/rentapp/views.py
+++ apps/rentapp/views.py
@@ -1,11 +1,23 @@
+from rest_framework.response import Response
+
+from rentassist.utils.views import AuthByTokenMixin
 from .models import Complaint, Rent
 from rest_framework import viewsets
 from .serializers import  ComplaintSerializer, RentSerializer
 # Create your views here.
      
-class RentViewSet(viewsets.ModelViewSet):
-    queryset = Rent.objects.all()
-    serializer_class = RentSerializer       
+class RentViewSet(AuthByTokenMixin, viewsets.ModelViewSet):
+    
+    def list(self, request, *args, **kwargs):
+        queryset = Rent.objects.all() 
+        serializer = RentSerializer(queryset, many=True)
+        if request.user.is_owner:
+            return Response(serializer.data)    
+        
+        if not request.user.is_owner:
+            queryset = Rent.objects.filter(tenant=request.user.tenant)
+            serializer = RentSerializer(queryset, many=True)
+            return Response(serializer.data)
 
 class CompalaintViewSet(viewsets.ModelViewSet):
     queryset = Complaint.objects.all()

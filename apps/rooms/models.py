c8479a00d50025a37f9a0075bf4e2199c5b636dd
Sunil Thapa <tsunil359@gmail.com>
2022-09-15 12:47:49 +0545

    remove rooms

--- apps/rooms/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth import get_user_model
-
-from users.models import Owner
-
-User = get_user_model()
-
-class Room(models.Model):
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, verbose_name='owner', related_name='rooms')

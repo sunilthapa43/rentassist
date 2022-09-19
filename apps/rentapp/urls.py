c29463c9cd8493f927b7f3908919318a1ece9856
Sunil Thapa <tsunil359@gmail.com>
2022-09-19 14:44:00 +0545

    remove rooms

--- apps/rentapp/urls.py
+++ apps/rentapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CompalaintViewSet, RentViewSet, RoomViewSet
+from .views import CompalaintViewSet, RentViewSet#, RoomViewSet
 
 
 
 router = DefaultRouter()
 router.register('rent', RentViewSet, basename= 'rent')
-router.register('rooms', RoomViewSet, basename='room')
+#router.register('rooms', RoomViewSet, basename='room')
 router.register('complaints',CompalaintViewSet, basename= 'complaint')
 # router.register('notifications', NotificationViewSet, basename= 'notification')
 
--- apps/rentapp/views.py
+++ apps/rentapp/views.py
@@ -1,9 +1,9 @@
 from rest_framework.response import Response
 
 from rentassist.utils.views import AuthByTokenMixin
-from .models import Complaint, Rent, Room
+from .models import Complaint, Rent#, Room
 from rest_framework import viewsets
-from .serializers import  ComplaintSerializer, RentSerializer, RoomSerializer
+from .serializers import  ComplaintSerializer, RentSerializer#, RoomSerializer
 from rentassist.utils.response import prepare_response
      
 class RentViewSet(AuthByTokenMixin, viewsets.ModelViewSet):
@@ -50,6 +50,6 @@ class CompalaintViewSet(AuthByTokenMixin, viewsets.ModelViewSet):
 
 
 
-class RoomViewSet(AuthByTokenMixin, viewsets.ModelViewSet):
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+# class RoomViewSet(AuthByTokenMixin, viewsets.ModelViewSet):
+#     queryset = Room.objects.all()
+#     serializer_class = RoomSerializer

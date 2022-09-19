775cdd9b617394647f26a9c016e384d97922ab2e
Sunil Thapa <tsunil359@gmail.com>
2022-09-19 19:17:24 +0545

    add device registration endpoint

--- apps/notification/urls.py
+++ apps/notification/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path , include
-
+from fcm_django.api.rest_framework import FCMDeviceAuthorizedViewSet
 from .views import NotificationAPIView
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [path('', NotificationAPIView.as_view(), name= 'notification')]
+router = DefaultRouter()
+router.register('devices', FCMDeviceAuthorizedViewSet, basename='devices')
+
+urlpatterns = [
+    path(
+        'notification/',
+        NotificationAPIView.as_view(),
+        name='notification',
+    ),
+    path('', include(router.urls)),
+]

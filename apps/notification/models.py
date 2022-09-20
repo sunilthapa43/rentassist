7259f39adfbaab31ca8ec973092bc1aaf7cabf50
Sunil Thapa <tsunil359@gmail.com>
2022-09-20 15:50:31 +0545

    push heroku

--- apps/notification/models.py
+++ apps/notification/models.py
@@ -20,7 +20,7 @@ class Notification(models.Model):
     created = models.DateTimeField(auto_now=True)
     tenant =  models.ForeignKey('users.Tenant', on_delete=models.CASCADE, verbose_name='Tenant', related_name='notification')
     is_read = models.BooleanField(default=False)
-    title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Title')
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Title')
     type = models.CharField(choices=NOTIFICATION_TYPES, max_length=30, verbose_name='type of notification', blank=False)
     target = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Notification target')
     deep_link = models.CharField(max_length=50, default = 'rentassist2021/api/notifications')
